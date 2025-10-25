c1ca5848ec2f2c7032913d54f0818a36d445dc83
nuex001 <edeemmanuelchizurumoke@gmail.com>
2025-10-25 22:25:07 +0100

    worked on the get user balance

--- src/App.jsx
+++ src/App.jsx
@@ -61,8 +61,6 @@ function App() {
       setStatus(`Error: ${error.message}`);
     }
   };
- 
-
 
   // 🪄 Handle connect popup
   const handleConnectClick = async () => {
@@ -75,8 +73,9 @@ function App() {
 
   // ❌ Handle disconnect with popup confirm
   const signDeposit = async () => {
- try {
-     const serializedTx = "AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAA4dVhUxzbYWhzUoW8op2eRj1dsbBkoz4M807VJKUpSFVioUDAnDiGwcaoRPMpMbzwSqwxeFGbHA/lPU/ypxDrTePCKSLlsIo0UcAWc6WTxmHArMH/YsAfNBhtgm8ff1qdHKOgcUmqUPPcxJbv6RJ7ytENRs/OsJaAlCNjnTap5cVIw/5siFVkiDRVOAxsccPPXXqGE95yQKx6q4gZpc8m0c9lLq45lPzh1wpDQkpfMFsbLqJauu2fyqBFWAuH5o6u4JnSB81fV/SrruUujHysUb+VoQ+ChwBQG2RudWZarR4WqqkP1Lj6G4GGInA4tCB/TOJQGmK7KYUgalOfZND8ib3+AU2LRAXylyujF7q7PfBjFLyLL34eEZ2P9HFom2COZz6+3AtZeSiwhw9lsMkuIszctQj9PV5ciVHiYwT+pBPO3t8bb6Ife3mBYa2q+/aRIB668TE/bopd4zp4RSn/mvPvHNd/W2rYk7Ka10Z5GMAvenzdI1nESFWsAlmvXULNw1BHgxJgcvQqnNZq3RjRuHuzEL9YMLzPVfai3xfAEc1AwMH3WJACrMqmfJDtuARdJUVI1/MSRV9afrx+oQWLTuTA5Ig05L1hIsKKQRxE8YtMGKVeLWEeisdOCXu89kbJygAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACMlyWPTiSJ8bs9ECkUjg2DC1oTmdr/EIQEjnvY2+n4WQMGRm/lIRcy/+ytunLDm+e8jOW7xfcSayxDmzpAAAAAsnDWf6mMUc8CEwUTWJYrrzV0K+1ZydlEXpwNDIXHzZG0P/on9df2SnTAmx8pWHneSwmrNt/J3VFLMhqns4zl6LYNyUUlvKL50j2zodlvpiQvg2vo9s63G91VKFoADQT/xvp6877brTo9ZfNqq8l0MbG75MLS9uDkfKYCA0UvXWHUKECzHcxG1IDLaPhNpk//K/8inSQSrfy5FmAW9bnb/goyk21h/qbYtgbnBsKhdLFrZ6WCSw/Uran4atTNmGV+BHnVW/IxwG7udMVuzmgVB/2xst6j9I5RArHNola8E48E6eEvvIToJskyzOniZAzOFVkMHGJzsJJXCLo7hSCwvAabiFf+q4GE+2h/Y0YYwDXaxDncGus7VZig8AAAAAABBqfVFxh70WY12tQEVf3CwMEkxo8hVnWl27rLXwgAAAAG3fbh12Whk9nL4UbO63msHLSF7V9bN5E6jPWFfv8AqSPZvldmHrANox8iHGY3KVjf/xdb1BN4xt53yuoDF7NQBREABQLAXBUAEQAJAwQXAQAAAAAAEAYAAgAaDxwBARgpHAEABAcKAhQaGBgTGBkFGQsGBwwUFQgZARwcEhkOGBcBAwoMDQkWGxwqwSCbM0HWnIEBAgAAACZkAAFWAf5kAQJYAgAAAAAAAA8AAAAAAAAAMgAAHAMCAAABCQ==";
+    try {
+      const serializedTx =
+        "AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAA4dVhUxzbYWhzUoW8op2eRj1dsbBkoz4M807VJKUpSFVioUDAnDiGwcaoRPMpMbzwSqwxeFGbHA/lPU/ypxDrTePCKSLlsIo0UcAWc6WTxmHArMH/YsAfNBhtgm8ff1qdHKOgcUmqUPPcxJbv6RJ7ytENRs/OsJaAlCNjnTap5cVIw/5siFVkiDRVOAxsccPPXXqGE95yQKx6q4gZpc8m0c9lLq45lPzh1wpDQkpfMFsbLqJauu2fyqBFWAuH5o6u4JnSB81fV/SrruUujHysUb+VoQ+ChwBQG2RudWZarR4WqqkP1Lj6G4GGInA4tCB/TOJQGmK7KYUgalOfZND8ib3+AU2LRAXylyujF7q7PfBjFLyLL34eEZ2P9HFom2COZz6+3AtZeSiwhw9lsMkuIszctQj9PV5ciVHiYwT+pBPO3t8bb6Ife3mBYa2q+/aRIB668TE/bopd4zp4RSn/mvPvHNd/W2rYk7Ka10Z5GMAvenzdI1nESFWsAlmvXULNw1BHgxJgcvQqnNZq3RjRuHuzEL9YMLzPVfai3xfAEc1AwMH3WJACrMqmfJDtuARdJUVI1/MSRV9afrx+oQWLTuTA5Ig05L1hIsKKQRxE8YtMGKVeLWEeisdOCXu89kbJygAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACMlyWPTiSJ8bs9ECkUjg2DC1oTmdr/EIQEjnvY2+n4WQMGRm/lIRcy/+ytunLDm+e8jOW7xfcSayxDmzpAAAAAsnDWf6mMUc8CEwUTWJYrrzV0K+1ZydlEXpwNDIXHzZG0P/on9df2SnTAmx8pWHneSwmrNt/J3VFLMhqns4zl6LYNyUUlvKL50j2zodlvpiQvg2vo9s63G91VKFoADQT/xvp6877brTo9ZfNqq8l0MbG75MLS9uDkfKYCA0UvXWHUKECzHcxG1IDLaPhNpk//K/8inSQSrfy5FmAW9bnb/goyk21h/qbYtgbnBsKhdLFrZ6WCSw/Uran4atTNmGV+BHnVW/IxwG7udMVuzmgVB/2xst6j9I5RArHNola8E48E6eEvvIToJskyzOniZAzOFVkMHGJzsJJXCLo7hSCwvAabiFf+q4GE+2h/Y0YYwDXaxDncGus7VZig8AAAAAABBqfVFxh70WY12tQEVf3CwMEkxo8hVnWl27rLXwgAAAAG3fbh12Whk9nL4UbO63msHLSF7V9bN5E6jPWFfv8AqSPZvldmHrANox8iHGY3KVjf/xdb1BN4xt53yuoDF7NQBREABQLAXBUAEQAJAwQXAQAAAAAAEAYAAgAaDxwBARgpHAEABAcKAhQaGBgTGBkFGQsGBwwUFQgZARwcEhkOGBcBAwoMDQkWGxwqwSCbM0HWnIEBAgAAACZkAAFWAf5kAQJYAgAAAAAAAA8AAAAAAAAAMgAAHAMCAAABCQ==";
 
       // Step 4: Deserialize and prepare main transaction
       const transactionBytes = Buffer.from(serializedTx, "base64");
@@ -100,34 +99,41 @@ function App() {
         preflightCommitment: "confirmed",
       });
       console.log("completed");
- } catch (error) {
-  console.log(error);
-  
- }
-      
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-const getVerifiedTokenList = async () => {
-  try {
-    console.log("Fetching Raydium token list...");
-    
-    // Raydium's official token list
-  const url = 'https://lite-api.jup.ag/tokens/v2/tag?query=verified';
-  const response = await axios.get(url);
-    const verifiedTokens = response.data; // Array of token objects
-    console.log(`Found ${verifiedTokens.length} verified tokens`);
-    console.log(verifiedTokens); //we will be needing the icon as image and the id as the token address or mint address, also symbol 
-    //you can choose to use all or just 100 or more.
-    
-    return verifiedTokens;
-  } catch (error) {
-    console.error("Error fetching Raydium tokens:", error);
-    return [];
-  }
-};
+  const getVerifiedTokenList = async () => {
+    try {
+      console.log("Fetching Raydium token list...");
+
+      // Raydium's official token list
+      const url = "https://lite-api.jup.ag/tokens/v2/tag?query=verified";
+      const response = await axios.get(url);
+      const verifiedTokens = response.data; // Array of token objects
+      console.log(`Found ${verifiedTokens.length} verified tokens`);
+      console.log(verifiedTokens); //we will be needing the icon as image and the id as the token address or mint address, also symbol
+      //you can choose to use all or just 100 or more.
+
+      return verifiedTokens;
+    } catch (error) {
+      console.error("Error fetching Raydium tokens:", error);
+      return [];
+    }
+  };
+  const getUserBalance = async () => {
+    try {
+    const lamports = await connection.getBalance(publicKey);
+        console.log(lamports / LAMPORTS_PER_SOL);
+    } catch (error) {
+      console.error("Error getting balance:", error);
+    }
+  };
 
 
-   useEffect(() => {
+
+  useEffect(() => {
     console.log(status);
   }, [status]);
   //
@@ -135,11 +141,11 @@ const getVerifiedTokenList = async () => {
     if (connected) {
       // console.log(connected);
       // getVerifiedTokenList(); ///comment this out to get the number of tokens verified tokens
+      getUserBalance(); ///get user balance
     } else {
       console.log("Connect wallet");
     }
   }, [connected]);
-  
 
   return (
     <>
